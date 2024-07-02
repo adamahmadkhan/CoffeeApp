d489b73eca95a1da5be63bb416b58e0fa10f04cf
adamahmadkhan <adamkhan8757@gmail.com>
2024-07-02 18:06:31 +0500

    added tableview to sidemenu

---
+++ CoffeeApp/Views/SideMenuCells.swift
@@ -0,0 +1,23 @@
+//
+//  SideMenuCells.swift
+//  CoffeeApp
+//
+//  Created by Adam Khan on 7/2/24.
+//
+
+import UIKit
+
+class SideMenuCells: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
