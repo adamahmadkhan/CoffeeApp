0207fe2bb3b633ae067f68255c1bb1a1accd8eac
adamahmadkhan <adamkhan8757@gmail.com>
2024-07-19 18:59:47 +0500

    type  one

--- CoffeeApp/SupportingClasses/CustomNavigationTransition.swift
+++ CoffeeApp/SupportingClasses/CustomNavigationTransition.swift
@@ -13,7 +13,7 @@ class CustomNavigationTransition: NSObject, UIViewControllerAnimatedTransitionin
     var isPresenting: Bool = true
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 1
+        return 2
     }
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         guard let toController = transitionContext.viewController(forKey: .to)!.view, let fromController = transitionContext.viewController(forKey: .from)!.view else {
@@ -23,22 +23,49 @@ class CustomNavigationTransition: NSObject, UIViewControllerAnimatedTransitionin
         let containerView = transitionContext.containerView
         if isPresenting {
             containerView.addSubview(toController)
-            toController.frame = CGRect(x: containerView.frame.width / 2, y: containerView.frame.height / 2 , width: 0, height: 0)
-            toController.layer.cornerRadius = 20
+            //toController.frame = CGRect(x: containerView.frame.width / 2, y: containerView.frame.height / 2 , width: 50, height: 50)
+            //toController.transform = CGAffineTransform(rotationAngle: CGFloat(360 * Double.pi) / 360)
+            toController.frame = CGRect(x: fromController.frame.width / 2, y: fromController.frame.height / 2 , width: 50 , height: 50)
+            toController.layer.cornerRadius = toController.frame.width / 2
             toController.layer.masksToBounds = true
-            UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
+            UIView.animate(withDuration: (transitionDuration(using: transitionContext)), animations: {
+               // toController.frame = CGRect(x: 0, y: fromController.bounds.midX, width: containerView.frame.width, height: containerView.frame.width)
                 toController.frame = CGRect(x: 0, y: 0, width: containerView.frame.width, height: containerView.frame.height)
+                toController.layer.cornerRadius = 0
+                //toController.layer.cornerRadius = toController.frame.width / 2
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi) / 360)
+//                }
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(360 * Double.pi) / 360)
+//                }
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi) / 360)
+//                }
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi) / 360)
+//                }
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(360 * Double.pi) / 360)
+//                }
+//                UIView.animate(withDuration: 0.5) {
+//                    toController.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi) / 360)
+//                }
                 //toController.superview?.alpha = 1
             }, completion: { _ in
                 transitionContext.completeTransition(true)
             })
 
         } else {
+            //fromController.transform = CGAffineTransform(rotationAngle: CGFloat(360 * Double.pi) / -360)
+            fromController.layer.cornerRadius = 0
             containerView.insertSubview(toController, belowSubview: fromController)
             toController.frame = CGRect(x: 0, y: 0, width: containerView.frame.width, height: containerView.frame.height)
             UIView.animate(withDuration: transitionDuration(using: transitionContext), animations: {
-                fromController.frame = CGRect(x: containerView.frame.width / 2, y: containerView.frame.height / 2, width: 0, height: 0)
+                fromController.frame = CGRect(x: containerView.frame.width / 2 , y: containerView.frame.height / 2, width: 80 , height: 80 )
+                //fromController.transform = CGAffineTransform(rotationAngle: CGFloat(0 * Double.pi) / 360)
                 //toController.superview?.alpha = 1
+                fromController.layer.cornerRadius = fromController.frame.height / 2
             }, completion: { _ in
                 transitionContext.completeTransition(true)
             })
